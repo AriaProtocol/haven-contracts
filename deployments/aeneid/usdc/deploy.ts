42df3d0db8929512f0f383dca0072dfd465e3e8c
theo6890 <mail@rcode.tech>
2025-12-17 19:40:28 +0100

    refactor: delete fauceteer and governance on aeneid

--- deployments/aeneid/usdc/deploy.ts
+++ deployments/aeneid/usdc/deploy.ts
@@ -28,9 +28,6 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   const ethers = deploymentManager.hre.ethers;
   const signer = await deploymentManager.getSigner();
 
-  // Deploy governance contracts
-  const { fauceteer, governor, timelock } = await cloneGov(deploymentManager);
-
   const USDC = await makeToken(deploymentManager, 10_000_000, 'USDC', 6, 'USDC');
   const WBTC = await makeToken(deploymentManager, 350, 'WBTC', 8, 'WBTC');
   const WETH = await makeToken(deploymentManager, 800, 'WETH', 18, 'WETH');
@@ -67,36 +64,6 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
     baseTokenPriceFeed: USDCPriceFeed.address,
     assetConfigs: [assetConfig0, assetConfig1],
   });
-  
-  // // Disable rewards for now
-  // const { rewards } = deployed;
-  // await deploymentManager.idempotent(
-  //   async () => (await WBTC.balanceOf(rewards.address)).eq(0),
-  //   async () => {
-  //     trace(`Sending some WBTC to CometRewards`);
-  //     const amount = exp(2_000_000, WBTCdecimals);
-  //     trace(await wait(WBTC.connect(signer).transfer(rewards.address, amount)));
-  //     trace(`WBTC.balanceOf(${rewards.address}): ${await WBTC.balanceOf(rewards.address)}`);
-  //   }
-  // );
 
-  // Mint some tokens
-  trace(`Attempting to mint as ${signer.address}...`);
-
-  await Promise.all(
-    [[USDC, 100_000 * 10 ** USDCdecimals], [WBTC, 7 * 10 ** WBTCdecimals], [WETH, 177 * 10 ** WETHdecimals]].map(([asset, units]) => {
-      return deploymentManager.idempotent(
-        async () => (await (asset as FaucetToken).balanceOf(fauceteer.address)).eq(0),
-        async () => {
-          const asset_ = asset as FaucetToken;
-          trace(`Minting ${units} ${await asset_.symbol()} to fauceteer`);
-          const amount = exp(Number(units), await asset_.decimals());
-          trace(await wait(asset_.connect(signer).allocateTo(fauceteer.address, amount)));
-          trace(`asset.balanceOf(${signer.address}): ${await asset_.balanceOf(signer.address)}`);
-        }
-      );
-    })
-  );
-
-  return { ...deployed, fauceteer };
+  return { ...deployed };
 }
