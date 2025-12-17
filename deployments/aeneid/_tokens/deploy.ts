b4d3c78c6169932e00434b198ec2ced5409d7a11
theo6890 <mail@rcode.tech>
2025-12-17 22:05:41 +0100

    chore: dedicated token deployment script for aeneid

---
+++ deployments/aeneid/_tokens/deploy.ts
@@ -0,0 +1,60 @@
+import {
+  Deployed,
+  DeploymentManager,
+} from "../../../plugins/deployment_manager";
+import { FaucetToken, SimplePriceFeed } from "../../../build/types";
+import {
+  DeploySpec
+} from "../../../src/deploy";
+
+async function makeToken(
+  deploymentManager: DeploymentManager,
+  amount: number,
+  name: string,
+  decimals: number,
+  symbol: string
+): Promise<FaucetToken> {
+  const mint = (BigInt(amount) * 10n ** BigInt(decimals)).toString();
+  return deploymentManager.deploy(symbol, "test/FaucetToken.sol", [
+    mint,
+    name,
+    decimals,
+    symbol,
+  ]);
+}
+
+async function makePriceFeed(
+  deploymentManager: DeploymentManager,
+  alias: string,
+  initialPrice: number,
+  decimals: number
+): Promise<SimplePriceFeed> {
+  return deploymentManager.deploy(alias, "test/SimplePriceFeed.sol", [
+    initialPrice * 1e8,
+    decimals,
+  ]);
+}
+
+export default async function deploy(
+  deploymentManager: DeploymentManager,
+  deploySpec: DeploySpec
+) {
+  const trace = deploymentManager.tracer();
+  const ethers = deploymentManager.hre.ethers;
+  const signer = await deploymentManager.getSigner();
+
+  const IP = await makeToken(
+    deploymentManager,
+    10_000,
+    "IP native",
+    18,
+    "IP"
+  );
+
+  await makePriceFeed(
+    deploymentManager,
+    "IP native:priceFeed",
+    2.35,
+    8
+  );
+}
