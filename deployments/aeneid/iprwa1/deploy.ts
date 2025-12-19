9cb3c0b02c659a3d202aaf3023271e241010ed72
theo6890 <mail@rcode.tech>
2025-12-19 18:02:07 +0100

    chore: iprwa1 deployment

---
+++ deployments/aeneid/iprwa1/deploy.ts
@@ -0,0 +1,33 @@
+import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
+import { DeploySpec, deployComet } from '../../../src/deploy';
+
+export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
+  deploySpec.excludeRewards = true;
+
+  // import from USDC market, as it was the first deployed market
+  const cometFactory = await deploymentManager.fromDep(
+    "cometFactory",
+    "aeneid",
+    "ip"
+  );
+  const cometProxyAdmin = await deploymentManager.fromDep(
+    "cometAdmin",
+    "aeneid",
+    "ip"
+  );
+  const configurator = await deploymentManager.fromDep(
+    "configurator",
+    "aeneid",
+    "ip"
+  );
+  const $configuratorImpl = await deploymentManager.fromDep(
+    "configurator:implementation",
+    "aeneid",
+    "ip"
+  );
+
+  // Deploy all Comet-related contracts
+  const deployed = await deployComet(deploymentManager, deploySpec);
+
+  return { ...deployed };
+}
